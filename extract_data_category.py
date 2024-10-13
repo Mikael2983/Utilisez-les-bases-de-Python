a5af98e50fee7f308b1b30846b1eda5609ba896f
Mikael Picolo <mikael.picolo@laposte.net>
2024-10-13 13:38:51 +0200

    extrait les urls des livres de toutes les pages de la catégorie sélectionnée.

--- extract_data_category.py
+++ extract_data_category.py
@@ -4,21 +4,40 @@ import csv
 from bs4 import BeautifulSoup
 
 csv_path = "C:/Users/mikae/Documents/formation/Project2/data_one_category.csv"
-url = "https://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
+url_base = "https://books.toscrape.com/catalogue/category/books/sequential-art_5/"
 
-#extraire la lsite des Url des livres de la catégorie
-url_livres=[]
-liste_livres = soup.find_all("div", class_="image_container")
+# Crétion de la liste des urls des pages de la catégorie
+url_pages = ["index.html"]
+url=url_base+url_pages[0]
+url_livres = []
 
-for product in liste_livres:
-    liste_a= product.find("a")  #extraction des balises a
-    url_livre = liste_a.get('href') # extraction de l'url du livre
-    url_livres.append(url_livre)
+while True :
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
 
+    # extraire la lsite des Url des livres de la catégorie
+    liste_livres = soup.find_all("div", class_="image_container")
+    for product in liste_livres:
+        liste_a = product.find("a")  # extraction des balises a
+        url_livre = liste_a.get('href')  # extraction de l'url du livre
+        url_livres.append(url_livre)
+    page_suivante = soup.find("li", class_="next")      #recherche d'une page suivante
+    if page_suivante:
+        liste_a= page_suivante.find("a")  #extraction des balises a
+        url_page = liste_a.get('href') # extraction de l'url de la page
+        url=url_base+url_page   #attribution de la nouvelle url
+    else:
+        break
 
 print(url_livres)
+print(len(url_livres))
+
+
+
+
+
+
+
 
 # # Si l'URL de l'image est relative, il faut la compléter par l'URL de base
 # base_url = "http://books.toscrape.com/"
