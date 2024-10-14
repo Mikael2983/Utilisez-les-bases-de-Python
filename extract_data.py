a82a3ad1a931b0f5246d28f24f9ec021f35db759
Mikael Picolo <mikael.picolo@laposte.net>
2024-10-14 10:40:29 +0200

    rajout de commentaires suppression de la recherche de la catégorie pour chaque livre

--- extract_data.py
+++ extract_data.py
@@ -11,27 +11,28 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 # Extraire les URLs des catégories
 categories_url={}
-categories_div = soup.find('ul', class_='nav-list')
+categories_div = soup.find('ul', class_='nav-list')  # je devrai nommer la variable categories_ul mais trop proche de categories.url je garde celle-ci
 categories = categories_div.find_all('a')
-for category in categories:
+for category in categories:  #stockage des url dans un dictionnaire  "nom de la catéorie" = "url de la catégorie"
     categories_url[category.getText().strip()] = site_url + category.get('href').replace("index.html","")
-del categories_url["Books"]
+del categories_url["Books"]  # cette catégorie n'en est pas réellement une car regroupe tous les livres du site
 
+#extraire tous les livres de chaque catégorie
 for category in categories_url:
-    csv_path = Path("data/livres_de_la_categorie_"+ category + ".csv").resolve()
-    url_base = categories_url[category]
-    url_page = "index.html"
-    url_livres = []
-    data_book = open(csv_path, 'w', encoding='utf-8')
-    # le <head> de la page indique charset=utf-8 pour éviter un problème d'encodage du csv (le caracère #) j'ai dû repréciser
+    url_livres = []     #initialisation de la liste qui contiendra les url de tous les livres de la catégorie
     # initialisation du fichier CVS avec le header
+    csv_path = Path("data/livres_de_la_categorie_" + category + ".csv").resolve()  # création du chemin et nom du fichier CVS incluant le nom de la catégorie
+    data_book = open(csv_path, 'w', encoding='utf-8') # création et ouverture du CSV
+    # le <head> de la page web indique charset=utf-8 pour éviter un problème d'encodage du csv (le caracère # en particulier) j'ai dû repréciser
     writer = csv.writer(data_book, delimiter=",")
-    writer.writerow(
+    writer.writerow(  # mise en place du header selon les exigences du projet
         ["product_page_url", "universal_ product_code", "title", "price_including_tax", "price_excluding_tax",
          "number_available", "product_description", "category", "review_rating", "image_url"])
 
-    while True:
-        url = url_base + url_page
+    url_base = categories_url[category]     #attribution de la premiere partie de l'URL qui contient le nom de la catégorie.
+    url_page = "index.html"                 #seconde partie de l'url pour la premiere page de la catégorie.
+    while True:                             # boucle tant qu'il trouve une page suivante.
+        url = url_base + url_page           #reconstitution de URL en fonction de la page suiavnte trouvée.
         page = requests.get(url)
         soup = BeautifulSoup(page.content, "html.parser")
         # extraire la liste des Urls des livres de la catégorie
@@ -46,7 +47,7 @@ for category in categories_url:
             url_page = liste_a.get('href')  # extraction de l'url de la page
         else:
             break
-
+    # extraire les données pour chaque livre à partir de la liste précédement établie
     for url in url_livres:
         url_livre = "https://books.toscrape.com/catalogue/" + url.replace("../", "")
         page = requests.get(url_livre)
@@ -75,29 +76,29 @@ for category in categories_url:
 
         # extraire la description
         descriptions = soup.find_all("p", class_="")  # liste des <p> sans class ( 1 seul sur la page du livre)
-        if descriptions:
+        if descriptions:   # Si le livre a bien une description
             data["product_description"] = descriptions[0].string  # extraction de la chaine de caractère constituant la description depuis la liste
-        else:
-            data["product_description"] =  "pas de description"
+        else:              # sinon préciser qu'il n'y a pas de description
+            data["product_description"] =  "pas de description trouvée"
+
         # extraire la categorie
-        ul = soup.find('ul', class_="breadcrumb")  # liste des élements de la liste de l'entête
-        categories = ul.find_all("li")[2]  # récupération du troisième élement de la liste correspondant à la catégorie
-        data["product_category"] = categories.find(
-            "a").string  # extraction de la chaine de caractère constituant le nom de la catégorie et stockage
+
+        # ul = soup.find('ul', class_="breadcrumb")  # liste des élements de la liste de l'entête
+        # categories = ul.find_all("li")[2]  # récupération du troisième élement de la liste correspondant à la catégorie
+        # data["product_category"] = categories.find("a").string  # extraction de la chaine de caractère constituant le nom de la catégorie et stockage
+
+        data["product_category"] = category #ancienne recherche est obsolète car à présent, elle est déjà faites plus haut dans le code
 
         # extraire le review rating
-        review_rating = soup.find('p', class_='star-rating')  # liste des <p> avec la class "star_rating"
-        data["rating_value"] = review_rating.get('class')[
-            1]  # Extraire le nombre d'étoile de la liste extraite de la classe de la balise <p>
+        review_rating = soup.find('p', class_='star-rating')  # liste des balises <p> avec la class "star_rating"
+        data["rating_value"] = review_rating.get('class')[1]  # Extraire le nombre d'étoile de la liste extraite de la classe de la balise <p>
 
         # extraire l'url de la cover
-        div_active_item = soup.find("div",
-                                    class_="item active")  # liste d'un seul élément des <div> avec la class item active
+        div_active_item = soup.find("div", class_="item active")  # liste d'un seul élément des <div> avec la class item active
         img_tag = div_active_item.find('img')  # extraction de la balise <img>
         img_url = img_tag['src']  # extraction de l'url de la source de l'image
 
         # Si l'URL de l'image est relative, il faut la compléter par l'URL de base
-
         if img_url.startswith("../"):
             img_url = site_url + img_url.replace("../", "")
 
@@ -109,7 +110,4 @@ for category in categories_url:
              data["Availability"], data["product_description"], data["product_category"], data["rating_value"],
              data["image_url"]])
 
-    data_book.close()
-
-
-
+    data_book.close() # fermeture du fichier CSV
