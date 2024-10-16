05b2dde0d8df0462934c63cce40d8bb80941fa28
Mikael Picolo <mikael.picolo@laposte.net>
2024-10-16 16:05:14 +0200

    call included in if __name__ == __main__:

--- main.py
+++ main.py
@@ -7,33 +7,38 @@ from pathlib import Path
 from bs4 import BeautifulSoup
 import utils
 
-url_site = "https://books.toscrape.com/"
 
-url_categories = utils.scrape_all_categories(url_site)
+if __name__ == __main__:
 
-for category in url_categories:
-    url_books = utils.scrape_category(category,url_categories[category])
-    data_book=[]
-    # Initialisation du fichier CSV avec le header
-    csv_path = Path("data/" + category + "/livres_de_la_categorie_" + category + ".csv").resolve()
+    url_site = "https://books.toscrape.com/"
 
-    with open(csv_path, 'w', encoding='utf-8', newline='') as backup_file:
-        writer = csv.writer(backup_file, delimiter=",")
-        writer.writerow(
-            ["product_page_url", "universal_product_code", "title", "price_including_tax", "price_excluding_tax",
-             "number_available", "product_description", "category", "review_rating", "image_url"])
+    url_categories = utils.scrape_all_categories(url_site)
 
-        for  url_book in url_books:
-            data_book = utils.scrape_book_data(url_book)
+    for category in url_categories:
+        url_books = utils.scrape_category(category,url_categories[category])
+        data_book=[]
+        # Initialisation du fichier CSV avec le header
+        csv_path = Path("data/" + category + "/livres_de_la_categorie_" + category + ".csv").resolve()
+
+        with open(csv_path, 'w', encoding='utf-8', newline='') as backup_file:
+            writer = csv.writer(backup_file, delimiter=",")
             writer.writerow(
-                [data_book["url product"], data_book["UPC"], data_book["title"], data_book["Price (incl. tax)"],
-                 data_book["Price (excl. tax)"], data_book["Availability"], data_book["product_description"],
-                 data_book["product_category"], data_book["rating_value"],data_book["image_url"]])
+                ["product_page_url", "universal_product_code", "title", "price_including_tax", "price_excluding_tax",
+                 "number_available", "product_description", "category", "review_rating", "image_url"])
 
-            image_file = requests.get(data_book["image_url"])
-            image= Image.open(BytesIO(image_file.content))
-            image_nom = "data/"+ category +"/images/"+re.sub(r'[\\/*?:"<>|]', ' ', data_book["title"])+"-"+data_book["UPC"]+".jpg"
-            image.save(image_nom)
+            for  url_book in url_books:
+                data_book = utils.scrape_book_data(url_book)
+                writer.writerow(
+                    [data_book["url product"], data_book["UPC"], data_book["title"], data_book["Price (incl. tax)"],
+                     data_book["Price (excl. tax)"], data_book["Availability"], data_book["product_description"],
+                     data_book["product_category"], data_book["rating_value"],data_book["image_url"]])
+
+                image_file = requests.get(data_book["image_url"])
+                image= Image.open(BytesIO(image_file.content))
+                image_nom = "data/"+ category +"/images/"+re.sub(r'[\\/*?:"<>|]', ' ', data_book["title"])+"-"+data_book["UPC"]+".jpg"
+                image.save(image_nom)
+
+
 
 
 
